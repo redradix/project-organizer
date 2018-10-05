138a0289f375cfd6eee0f955a65e4dc46f28ab71
Andres Martin <monkeyandres@protonmail.com>
2018-10-05 13:40:45 +0200

    Adding sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,22 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    // applyMiddleware(thunk),
+    applyMiddleware(sagaMiddleware),
     typeof window === "object" &&
     typeof window.devToolsExtension !== "undefined"
       ? window.devToolsExtension()
       : f => f
   )
 );
+
+sagaMiddleware.run();
 
 export default store;
