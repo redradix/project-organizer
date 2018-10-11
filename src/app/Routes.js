3f48a79e820f5f4b1725f988121a552db6df9b2d
Andres Martin <monkeyandres@protonmail.com>
2018-10-11 15:55:53 +0200

    Fixed calendar import

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import Calendar from './calendar/Calendar'
+import Calendar from './Calendar/Calendar'
 import Project from '../ui/views/Project'
 import Assignments from '../ui/views/Assignments'
 import AssignmentDetails from '../app/assignments/AssignmentDetails'
